ccb6004b1f5bd4fc6e341d9a68558aa93c22b665
FelipeCarlota <47505973+FelipeCarlota@users.noreply.github.com>
2023-07-31 12:32:07 -0300

    Adicionado backgrounds à pasta publica

---
+++ src/components/Backgrounds/index.tsx
@@ -0,0 +1,9 @@
+import City from '../../img/background-header.jpg'
+import AppSection from '../../img/background-breve-app.png'
+import TestimonySection from '../../img/background-depoimentos.jpg'
+
+export {
+  City,
+  AppSection,
+  TestimonySection
+}
